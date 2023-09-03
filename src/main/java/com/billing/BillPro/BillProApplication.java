2e2a5371434cafb125522e545ce9975931249b73
rakeshramesan <rakesh.ramesan@tmhighland.com>
2023-09-03 13:54:56 -0500

    initial commit

---
+++ src/main/java/com/billing/BillPro/BillProApplication.java
@@ -0,0 +1,13 @@
+package com.billing.BillPro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillProApplication.class, args);
+	}
+
+}
